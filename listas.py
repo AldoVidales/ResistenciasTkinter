6ca0b0b638ab5169f92600bce1b64288b182a65a
¨Aldo¨ <aldo.vidales@yahoo.com>
2022-06-06 14:12:02 -0500

    v(6/6/22)

--- listas.py
+++ listas.py
@@ -1,6 +1,8 @@
 from calendar import month
 import imp
 from msilib.schema import ComboBox
+import string
+from time import time
 from tkinter import *
 from tkinter import ttk
 from turtle import color
@@ -8,84 +10,51 @@ from turtle import color
 def scrollbar(root):
 
     def cambiar():
-        c1=StringVar()
+        
+        csum=StringVar()
         color1=str(list1.get())
-        if color1=="Black":
-            Label(root,text="",bg="black",height=2,width=3).place(x=100,y=120)
-            c1="0"
-        elif color1=="Brown":
-            Label(root,text="",bg="brown",height=2,width=3).place(x=100,y=120)
-            c1="1"
-        elif color1=="Red":
-            Label(root,text="",bg="red",height=2,width=3).place(x=100,y=120)
-            c1="2"
-
-        elif color1=="Orange":
-            Label(root,text="",bg="orange",height=2,width=3).place(x=100,y=120)
-            c1="3"
-
-        elif color1=="Yellow":
-            Label(root,text="",bg="yellow",height=2,width=3).place(x=100,y=120)
-            c1="4"
-
-
-        elif color1=="Green":
-            Label(root,text="",bg="green",height=2,width=3).place(x=100,y=120)
-            c1="5"
-
-        elif color1=="Blue":
-            Label(root,text="",bg="blue",height=2,width=3).place(x=100,y=120)
-            c1="6"
-        elif color1=="Violet":
-            Label(root,text="",bg="purple",height=2,width=3).place(x=100,y=120)
-            c1="7"
-        elif color1=="Gray":
-            Label(root,text="",bg="gray",height=2,width=3).place(x=100,y=120)
-            c1="8"
-        elif color1=="White":
-            Label(root,text="",bg="white",height=2,width=3).place(x=100,y=120)
-            c1="9"
+        if list1.get()!= 0:
+            Label(root,text="",bg=color1,height=2,width=3).place(x=100,y=120)
+            c1=list1['values'].index(list1.get())
+    
 
         color2=str(list2.get())
-        if color2=="Black":
-            Label(root,text="",bg="black",height=2,width=3).place(x=100+50,y=120)
-            c2="0"
-        elif color2=="Brown":
-            Label(root,text="",bg="brown",height=2,width=3).place(x=100+50,y=120)
-            c2="1"
-        elif color2=="Red":
-            Label(root,text="",bg="red",height=2,width=3).place(x=100+50,y=120)
-            c2="2"
+        if list2!= 0:
+            Label(root,text="",bg=color2,height=2,width=3).place(x=100+50,y=120)
+            c2=list2['values'].index(list2.get())
 
-        elif color2=="Orange":
-            Label(root,text="",bg="orange",height=2,width=3).place(x=100+50,y=120)
-            c2="3"
+        color4=str(list4.get())
+        if list4!= 0:
+            Label(root,text="",bg=color4,height=2,width=3).place(x=100+100,y=120)
+            c3=list4['values'].index(list4.get())
+            values=[1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000]
 
-        elif color2=="Yellow":
-            Label(root,text="",bg="yellow",height=2,width=3).place(x=100+50,y=120)
-            c2="4"
+        csum=str(c1)+str(c2)
+        csum=int(csum)
+        csum=str(csum*values[c3])
+        csum=int(csum)
+        def resultado(csum):
+            Label(root,text="Resultado: "+str(csum),bg="white",height=2,width=20).place(x=10,y=300)
 
 
-        elif color2=="Green":
-            Label(root,text="",bg="green",height=2,width=3).place(x=100+50,y=120)
-            c2="5"
+        resultado(csum)
+        
+        
+            
+            
 
-        elif color2=="Blue":
-            Label(root,text="",bg="blue",height=2,width=3).place(x=100+50,y=120)
-            c2="6"
-        elif color2=="Violet":
-            Label(root,text="",bg="purple",height=2,width=3).place(x=100+50,y=120)
-            c2="7"
-        elif color2=="Gray":
-            Label(root,text="",bg="gray",height=2,width=3).place(x=100+50,y=120)
-            c2="8"
-        elif color2=="White":
-            Label(root,text="",bg="white",height=2,width=3).place(x=100+50,y=120)
-            c2="9"
+           
+
+          
+
+            
+        
 
 
 
-        Label(root, text=f'Valor de resistencia {c1} {c2} ohms').place(x=10, y=300)
+        
+      
+    
         
       
             
@@ -99,16 +68,21 @@ def scrollbar(root):
     # get first 3 letters of every month name
     list1['values'] = ('Black', 'Brown','Red','Orange','Yellow' ,'Green','Blue','Violet','Gray','White')
     list2=ttk.Combobox(root, values=["Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Gray","White"],width=10)
+
+    list4=ttk.Combobox(root, values=["Black","Brown","Red","Orange","Yellow","Green","Blue","Violet","Gray","White"],width=10)
     # prevent typing a value
     list1['state'] = 'readonly'
     list2['state'] = 'readonly'
+    list4['state'] = 'readonly'
     list1.current(0)
     list2.current(0)
+    list4.current(0)
     
 
     # place the widget
     list1.place(x=10, y=200)
     list2.place(x=100, y=200)
+    list4.place(x=200, y=200)
     Button(root, text="Cambiar", command=cambiar).place(x=10, y=250)
     
     
