52dfe90dc1711d8ae5503e31001c6ea62a29344d
¨Aldo¨ <aldo.vidales@yahoo.com>
2022-06-06 15:08:22 -0500

    alpha4

--- credits.py
+++ credits.py
@@ -2,8 +2,9 @@ from tkinter import *
 def credits():
    filewin = Toplevel()
    filewin.geometry('200x200')
+   
    filewin.resizable(False, False)
-   credits = Label(filewin, text="Credits:"+'\n'+"Programmer:Alrift "+'\n'+"Name:Aldo Vidales "+'\n'+"Email:aldo.vidales@yahoo.com "+'\n'+"Date:3/6/2022 "+'\n'+"Version:1.0 "+'\n'+"License:MIT "+'\n')
+   credits = Label(filewin, text="Credits:"+'\n'+"Programmer:Aldo Vidales "+'\n'+"Name:Calculadora de resistencias "+'\n'+"Email:aldo.vidales@yahoo.com "+'\n'+"Date:3/6/2022 "+'\n'+"Version:1.0 "+'\n'+"License:MIT "+'\n')
    credits.pack()
 
 def help():
